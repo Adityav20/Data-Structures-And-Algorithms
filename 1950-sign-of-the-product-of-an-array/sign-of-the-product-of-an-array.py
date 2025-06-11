c1344f75880248c6adf7bd19420e5824d874f82f
Adityav20 <55047734+Adityav20@users.noreply.github.com>
2025-06-11 23:01:22 +0200

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.65%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.py
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.py
@@ -1,6 +1,12 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        product = nums[0]
+        sign = 1
+        for num in nums:
+            if num < 0: sign *= -1
+            elif num ==0 : return 0
+        return sign
+        
+        """product = nums[0]
         for i in range(len(nums)-1,0,-1):
             product *= nums[i]
 
@@ -9,5 +15,5 @@ class Solution:
         elif product < 0:
             return -1
         else:
-            return 0
+            return 0 """
             
